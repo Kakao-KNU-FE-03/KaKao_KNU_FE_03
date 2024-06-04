f1e268fd6264c0cae4adcc6a7f6ecc97acd4e6bf
Dobbymin <gmin0701@knu.ac.kr>
2024-06-04 20:38:32 +0900

    Feat: fizz buzz 최종 구현

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,15 @@
-export default function fizzbuzz() {
-  for (let i = 0; i <= 100; i++) {
-    i % 3 === 0 && i % 5 === 0 ? console.log("FizzBuzz") : null
-  }
-}
+// 1부터 100까지의 숫자를 포함하는 배열 생성
+const nums = Array.from({ length: 100 }, (_, i) => i + 1);
+
+// FizzBuzz 로직을 적용하여 결과 출력
+nums.map((num) => {
+	if (num % 3 === 0 && num % 5 === 0) {
+		console.log('FizzBuzz');
+	} else if (num % 3 === 0) {
+		console.log('Fizz');
+	} else if (num % 5 === 0) {
+		console.log('Buzz');
+	} else {
+		console.log(num);
+	}
+});
